f2f9d6ed6b981cd958d26ea217f51b576d13e9b3
Zinfidel <yamkoo@gmail.com>
2014-11-08 16:48:17 -0700

    Implemented parsetable.

---
+++ parsetable.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from cfg import *
+
+class ParseTable:
+    """ Represents a parse table. """
+
+    def __init__(self, grammar):
+        self.table = dict()
+        self.ll1 = True
+
+        for nonTerminal in grammar.nonTerminals:
+            self.table[nonTerminal] = dict()
+            for terminal in grammar.terminals:
+
+                productions = grammar.productions[nonTerminal]
+                productions = [x for x in productions if terminal in x]
+                if len(productions) > 1: self.ll1 = False
+                self.table[nonTerminal][terminal] = productions
