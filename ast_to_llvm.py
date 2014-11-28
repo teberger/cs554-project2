17c10d98a9dd117d96314e67234435eb3f7c9e9e
Taylor <tberge01@cs.unm.edu>
2014-11-28 10:24:19 -0700

    fixed a slight error in the grammar

--- ast_to_llvm.py
+++ ast_to_llvm.py
@@ -1,13 +1,41 @@
 from parser import Parser
 from cfg import Grammar
 from llvm import *
+from ll1_tools import (first,follows)
 
-def ast_to_cst(ast):
-    pass
+def ast_to_llvm(ast):
+    '''
+    This function creates LLVM code for the toy language
+    defined in ./testdata/homework1_grammar.txt
+    
+    It assumes the parser has the general structure of
+    a AST wrt the Arithmetic language in the aforementioned
+    grammar
 
-def cst_to_llvm(cst):
+    :param ast: a RoseTree that represents the AST of 
+                the parsed Arithmetic language
+    :return: LLVM code that would compile into a linux x86 
+             executable
+    '''
+
+    
     pass
 
 if __name__ == '__main__':
-    g = Grammar('./testdata/html.txt')
+    g = Grammar('./testdata/homework1_grammar.txt')
     x = Parser(g)
+    
+    for c in follows(g):
+        print c, ' -> ', follows(g)[c]
+    
+
+    x.ll1_parse([('while', 'while'), 
+                 ('not', 'not'), 
+                 ('true','true'), 
+                 ('do','do'), 
+                 ('var', 'x'),
+                 ('aop', '+'),
+                 ('num','123'),
+                 ('od', 'od')
+                ])
+    
